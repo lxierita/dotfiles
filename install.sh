d25854ec02a93011112d3899892fedcabbb9b19d
Lang Xie <53173457+lxierita@users.noreply.github.com>
2022-01-13 10:14:44 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+# Change shell
+chsh -s /bin/zsh
+
 # Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/lxierita/.zprofile

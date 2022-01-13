27a2f4c6806af5ec0bbf804454356f6c3e573540
Lang Xie <53173457+lxierita@users.noreply.github.com>
2022-01-13 11:45:16 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,6 +12,13 @@ brew install libevent ncurses tmux node yarn zsh curl
 # Casks
 brew install iterm2 webstorm goland
 
+# Launch apps from cli
+echo ' #!/bin/sh \
+open -na "WebStorm.app" --args "$@" ' >> /usr/local/bin/webstorm
+
+echo ' #!/bin/sh \
+open -na "GoLand.app" --args "$@" ' >> /usr/local/bin/goland
+
 # Config zsh
 curl -L https://iterm2.com/shell_integration/zsh \
 -o ~/.iterm2_shell_integration.zsh
